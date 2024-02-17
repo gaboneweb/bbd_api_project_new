632d55f6e0cb077ef8519147326c4794d63fb459
gaboneweb <bulelanigabonewe@gmail.com>
2024-02-17 09:28:25 +0200

    Added the required dependencies

--- UkukhulaAPI/obj/Debug/net8.0/UkukhulaAPI.AssemblyInfo.cs
+++ UkukhulaAPI/obj/Debug/net8.0/UkukhulaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UkukhulaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31def4d3babdfb9cfea812c45e1e2dd8a62a560a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a381f2ef690cb306f1a29137d85c4ee82b93df")]
 [assembly: System.Reflection.AssemblyProductAttribute("UkukhulaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UkukhulaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
