36c900661c39e5a3d0217a5f1071e48f5a98d54d
Manqoba-stack <manyoba1997@gmail.com>
2024-02-18 15:59:43 +0200

    use as example

--- UkukhulaAPI/Data/Services/ApplicationsService.cs
+++ UkukhulaAPI/Data/Services/ApplicationsService.cs
@@ -14,6 +14,9 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using UkukhulaAPI.Controllers.Request;
 namespace UkukhulaAPI.Data.Services;
 
 public class ApplicationsService
@@ -46,7 +49,7 @@ public class ApplicationsService
                 CourseOfStudy = app.Student.CourseOfStudy,
                 // YearBudget = app.BursaryDetails.YearBudget,
                 // CapAmountPerStudent = app.BursaryDetails.CapAmountPerStudent,
-                race=app.Student.Race.RaceName,
+                race = app.Student.Race.RaceName,
                 Status = app.Status.Status,
                 Department = app.HeadOfDepartment.Department.DepartmentName,
                 HeadOfDepartmentUniversity = app.HeadOfDepartment.University.UniversityName,
@@ -63,11 +66,112 @@ public class ApplicationsService
         return JsonSerializer.Serialize(applicationVms, options);
     }
 
-    public class AddUserRequest
+    public bool InsertStudentApplication([FromBody] AddStudentApplicationRequest studentApplicationRequest)
     {
-        public required UserRegistrationVm User { get; set; }
-        public required ContactVm Contact { get; set; }
+        var user = _context.Users.FirstOrDefault(e => e.Idnumber == studentApplicationRequest.User.Idnumber);
+        // var user = _context.Users.Find(BigInteger.Parse(studentApplicationRequest.User.Idnumber));
+        if (user != null)
+        {
+            return false;
+        }
+        using (var cx = _context)
+        {
+            DateTime today = DateTime.Today;
+
+            try
+            {
+                var applicantContact = new Contact()
+                {
+                    Email = studentApplicationRequest.Contact.Email,
+                    ContactNumber = studentApplicationRequest.Contact.ContactNumber
+                };
+                cx.Add(applicantContact);
+                cx.SaveChanges();
+
+                var applicantInfo = new User()
+                {
+                    FirstName = studentApplicationRequest.User.FirstName,
+                    LastName = studentApplicationRequest.User.LastName,
+                    Idnumber = studentApplicationRequest.User.Idnumber,
+                    ContactId = applicantContact.ContactId
+                };
+                cx.Add(applicantInfo);
+                cx.SaveChanges();
+
+
+                var hod = new HeadOfDepartment()
+                {
+                    HeadOfDepartmentId = studentApplicationRequest.User.HODId
+                };
+
+                var _newUser = cx.Users.FirstOrDefault(e => e.Idnumber == studentApplicationRequest.User.Idnumber);
+                var _hod = cx.HeadOfDepartments.FirstOrDefault(e => e.HeadOfDepartmentId == studentApplicationRequest.User.HODId);
+                if (_newUser == null || _hod == null)
+                {
+                    return false;
+                }
+
+                var asStudent = new Student()
+                {
+                    CourseOfStudy = studentApplicationRequest.User.CourseOfStudy,
+                    UniversityId = _hod.UniversityId,
+                    RaceId = studentApplicationRequest.User.race,
+                    UserId = _newUser.UserId,
+                    DepartmentId = _hod.DepartmentId
+
+
+                };
+                cx.Add(asStudent);
+                cx.SaveChanges();
+
+                var _newstudent = cx.Students.FirstOrDefault(e => e.User.Idnumber == studentApplicationRequest.User.Idnumber);
+
+                if (_newstudent == null)
+                {
+                    return false;
+                }
+
+                var applicantionInfo = new StudentBursaryApplication()
+                {
+                    StudentId = _newstudent.StudentId,
+                    HeadOfDepartmentId = _hod.HeadOfDepartmentId,
+                    ApplicationDate = DateOnly.FromDateTime(today),
+                    ApplicationMotivation = studentApplicationRequest.User.ApplicationMotivation,
+                    AverageMark = studentApplicationRequest.User.averageMark,
+                    ApplicationRejectionReason = "N/A",
+                    BursaryAmount = studentApplicationRequest.User.BursaryAmount,
+                    StatusId = 1,
+                    BursaryDetailsId = DateTime.Now.Year,
+
+
+                };
+                // cx.Add(hod);
+                // cx.SaveChanges();
+
+
+                cx.Add(applicantionInfo);
+                cx.SaveChanges();
+
+
+
+
+
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        return true;
+
     }
+
+    // public class AddStudentApplicationRequest
+    // {
+    //     public required StudentRegistrationVm User { get; set; }
+    //     public required ContactVm Contact { get; set; }
+    // }
     // public bool findApplication(UserRegistrationVm user, ContactVm contact)
     // {
 
